54c172d59b67115fecf7f7cb7c15226047710ea2
RAGUNATH A <168917392+Blackrose143@users.noreply.github.com>
2026-05-21 11:41:23 +0530

    Time: 179 ms (53%), Space: 187.2 MB (17.5%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,11 +1,7 @@
 struct node{
-    vector<bool> s1;
-    vector<bool> s2;
     vector<node*> childs;
     node(){
-        childs.resize(26);
-        s1.resize(26);
-        s2.resize(26);
+        childs.resize(10);
     }
 };
 
@@ -21,10 +17,6 @@ struct trie{
             int id=c-'0';
             if(cur->childs[id]==nullptr)
                 cur->childs[id]=new node();
-            if(b)
-                cur->s1[id]=1;
-            else
-                cur->s2[id]=1;
             cur=cur->childs[id];
         }
     }
@@ -34,7 +26,7 @@ struct trie{
         int cnt=0;
         for(char c:s){
             int id=c-'0';
-            if(cur->childs[id]==nullptr || !cur->s1[id] || !cur->s2[id])
+            if(cur->childs[id]==nullptr)
                 return cnt;
             cur=cur->childs[id];
             cnt+=1;
@@ -52,14 +44,6 @@ public:
         for(int i:arr1){
             string s=to_string(i);
             t.insert(s,true);
-        }
-        for(int i:arr2){
-            string s=to_string(i);
-            t.insert(s,false);
-        }
-        for(int i:arr1){
-            string s=to_string(i);
-            ans=max(ans,t.search(s));
         }
         for(int i:arr2){
             string s=to_string(i);
