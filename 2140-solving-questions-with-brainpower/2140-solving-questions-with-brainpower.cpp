7e5098e3230753f0eea52e75bffee64587cfa67a
RAGUNATH A <168917392+Blackrose143@users.noreply.github.com>
2026-06-16 12:16:10 +0530

    Time: 18 ms (10.35%), Space: 124.5 MB (47.87%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int n ;
-    long long dp[100005];
-    long long fun( int i , vector<vector<int>>&arr){
 
-        if( i >= n )return 0;
-        long long ans = -1e18;
-        if(dp[i] !=-1) return dp[i];
-        ans = max( ans , fun( i + 1  , arr  )  );
-        ans = max( ans , fun( i + arr[i][1] + 1  , arr ) + arr[i][0]);
-
-        return dp[i]= ans;
+    long long n,dp[100032];
+    long long fun( int i , vector<vector<int>>& arr ) {
+        if(i >= n)
+            return 0;
+        if(dp[i]!=-1)
+            return dp[i];
+        long long ans=-1e18;
+        ans = max( ans , fun(i+1,arr));
+        ans = max( ans , fun(i+arr[i][1]+1,arr) + arr[i][0] );
+        return dp[i] = ans;
     }
     long long mostPoints(vector<vector<int>>& questions) {
-            n = questions.size();
-            memset( dp , -1 ,sizeof(dp));
-            long long res = fun( 0 , questions);
-            return res ;
+        n=questions.size();
+        memset(dp,-1,sizeof(dp));
+        return fun(0,questions);
     }
 };
