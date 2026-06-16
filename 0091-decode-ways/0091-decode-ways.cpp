b928e8b9273a561e6d8b44970dd682927fe6abd2
RAGUNATH A <168917392+Blackrose143@users.noreply.github.com>
2026-06-16 11:41:25 +0530

    Time: 3 ms (16.08%), Space: 10.9 MB (12.74%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
 
-    int dp[102];
-    int n ;
-    int rec(int i,string& s){
-        if( i == n ){
+    int n,dp[132];
+    int fun( int i , string s){
+        if(i==n)
             return 1;
-        }
-
-        int ans = 0 ;
-
-        if( dp[i] != -1) 
+        if(i>n)
+            return 0;
+        if(dp[i]!=-1)
             return dp[i];
-
-        int a = 0 ;
-        if( i < n )
-            a = (s[i] - '0');
-        if( a != 0 )
-            ans += rec( i + 1, s );
-        // 226
-        //a = 2 
-        // b = 22 
-        if( i + 1 < n && a == 1 ||a == 2   ){
-            int b = a * 10 + s[i+1] - '0';
-            if( b <= 26 )
-            ans += rec( i + 2 , s ) ; 
+        int ways=0,a=(s[i]-'0');
+        if(a != 0)
+            ways += fun( i+1 , s);
+        if(i+1 < n && a==1 || a==2){
+            int val=10*a+(s[i+1]-'0');
+            if(val<=26)
+                ways += fun(i+2,s);
         }
-        return dp[i] =  ans;
+        return dp[i]=ways;
     }
     int numDecodings(string s) {
-        n =s.size();
-        memset(dp,-1,sizeof(dp));
-        int res = rec(0,s);
-        char c = '4';
-        int a = c - '0';
-        cout<<a<<endl;
-
-        return res;
+        n=s.length();
+        memset(dp,-1,sizeof(dp));;
+        return fun(0,s);
     }
 };
