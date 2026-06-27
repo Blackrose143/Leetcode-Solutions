3a7b075b1982e2051f1cd15577b335333b0e688e
RAGUNATH A <168917392+Blackrose143@users.noreply.github.com>
2026-06-27 14:38:05 +0530

    Time: 0 ms (100%), Space: 9.7 MB (63.07%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            vector<int> res;
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i)
+                    res.push_back(1);
+                else
+                    res.push_back((ans[i-1][j-1]+ans[i-1][j]));
+            }
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
