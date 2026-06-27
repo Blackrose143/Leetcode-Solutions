866ae7cbb7cd30d84bc1a214a43f6b48a6b7ebc8
RAGUNATH A <168917392+Blackrose143@users.noreply.github.com>
2026-06-27 14:04:54 +0530

    Time: 1 ms (34.2%), Space: 21.1 MB (18.52%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,27 +2,22 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size(),m=matrix[0].size();
-        vector<vector<int>> ans(n,vector<int>(m,1));
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                ans[i][j]=matrix[i][j];
+        set<int> r,c;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    ans[i][j]=0;
-                    for(int k=i+1;k<n;k++)
-                        ans[k][j]=0;
-                    for(int k=i-1;k>=0;k--)
-                        ans[k][j]=0;
-                    for(int k=j+1;k<m;k++)
-                        ans[i][k]=0;
-                    for(int k=j-1;k>=0;k--)
-                        ans[i][k]=0;
+                    r.insert(i);
+                    c.insert(j);
                 }
             }
         }
-        for(int i=0;i<n;i++)
+        for(int i:r)
             for(int j=0;j<m;j++)
-                matrix[i][j]=ans[i][j];
+                matrix[i][j]=0;
+        
+        for(int j:c)
+            for(int i=0;i<n;i++)
+                matrix[i][j]=0;
+        
     }
 };
