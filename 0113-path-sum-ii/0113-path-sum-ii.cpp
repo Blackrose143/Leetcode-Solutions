4e22d1c2e930308b19c688ff978e279d9a6a652f
RAGUNATH A <168917392+Blackrose143@users.noreply.github.com>
2026-08-23 13:06:01 +0530

    Time: 0 ms (100%), Space: 21.1 MB (41.06%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -12,35 +12,36 @@
 class Solution {
 public:
 
-    int k;
+    int t;
     vector<vector<int>> ans;
-    void fun(TreeNode* root,vector<int>& nums,int sum) {
-        if(root==NULL)
-            return;
-        if(root->left==NULL && root->right==NULL){
-            nums.push_back(root->val);
-            sum += root->val;
-            if(sum==k)
-                ans.push_back(nums);
-            nums.pop_back();
-            return;
+    void fun(TreeNode* root,vector<int>& cur) {
+    
+        if(!root->left && !root->right) {
+            int sum = accumulate(cur.begin(),cur.end(),0LL);
+            if(sum==t)
+                ans.push_back(cur);
+            return ;
         }
-        if(root->left!=NULL){
-            nums.push_back(root->val);
-            fun(root->left,nums,sum+root->val);
-            nums.pop_back();
+
+        if(root->left) {
+            cur.push_back(root->left->val);
+            fun(root->left,cur);
+            cur.pop_back();
         }
-        if(root->right!=NULL){
-            nums.push_back(root->val);
-            fun(root->right,nums,sum+root->val);
-            nums.pop_back();
+        if(root->right) {
+            cur.push_back(root->right->val);
+            fun(root->right,cur);
+            cur.pop_back();
         }
     }
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        k = targetSum;
-        vector<int> nums;
-        fun(root,nums,0);
+        if(!root)
+            return {};
+        t = targetSum;
+        vector<int> res;
+        res.push_back(root->val);
+        fun(root,res);
         return ans;
     }
 };
