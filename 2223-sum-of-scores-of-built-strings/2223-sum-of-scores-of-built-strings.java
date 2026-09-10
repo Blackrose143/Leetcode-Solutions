cd30946d28291bfecad832c537cdb939ab7b757f
RAGUNATH A <168917392+Blackrose143@users.noreply.github.com>
2026-09-10 19:17:36 +0530

    Time: 17 ms (24.47%), Space: 47.9 MB (12.08%) - LeetHub

--- 2223-sum-of-scores-of-built-strings/2223-sum-of-scores-of-built-strings.java
+++ 2223-sum-of-scores-of-built-strings/2223-sum-of-scores-of-built-strings.java
@@ -4,20 +4,21 @@ class Solution {
         int l = 0, r = 0;
         long ans = 0;
 
-        List<Integer> z = new ArrayList<>(Collections.nCopies(n,0));
+        int[] z = new int[n];
+
         for(int i=1;i<n;i++) {
+
             if(i<=r)
-                z.set(i,Math.min(z.get(i-l),r-i+1));
+                z[i] = Math.min(z[i-l],r-i+1);
 
-            while( i + z.get(i) < n && s.charAt(z.get(i)) == s.charAt(i+z.get(i)) )
-                z.set(i,z.get(i)+1);
+            while(i+z[i] < n && s.charAt(z[i]) == s.charAt(i+z[i]))
+                z[i]++;
 
-            if( i + z.get(i) - 1 > r ) {
+            if(i+z[i]-1 > r) {
                 l = i;
-                r = i + z.get(i) - 1;
+                r = i+z[i]-1;
             }
-
-            ans += z.get(i);
+            ans += z[i];
         }
         return n+ans;
     }
