50e0e1df85e88cabd3a49715fd9961441781a6d7
RAGUNATH A <168917392+Blackrose143@users.noreply.github.com>
2026-09-11 20:11:13 +0530

    Time: 111 ms (62.71%), Space: 66.6 MB (40.24%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-
     int findLeastNumOfUniqueInts(vector<int>& nums, int k) {
+        
         unordered_map<int,int> mp;
         for(int i:nums)
             mp[i]++;
-        if(k==0)
-            return mp.size();
-        vector<pair<int,int>> vc;
-        for(auto pi:mp) {
-            vc.push_back({pi.second,pi.first});
-        }
-        sort(vc.begin(),vc.end());
 
-        int cnt = 0;
-        for(auto v:vc) {
-            int x=v.second;
-            while(true) {
-                mp[x]--;
-                cnt++;
-                if(mp[x]==0) {
-                    mp.erase(x);
-                    break;
-                }
-                if(cnt==k)
-                    break;
-            }
-            if(cnt==k)
-                break;
+        vector<int> freq;
+        for(auto pi:mp) {
+            int F = pi.second;
+            freq.push_back(F);
         }
-        return mp.size();
+
+        sort(freq.begin(),freq.end());
+
+        int n = freq.size();
+        for(int i=0;i<n;i++) {
+            k -= freq[i];
+            if(k==0)
+                return n-i-1;
+            if(k<0)
+                return n-i;
+        }
+        return n;
     }
 };
