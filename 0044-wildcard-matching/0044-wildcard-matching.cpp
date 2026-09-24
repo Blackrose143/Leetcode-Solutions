3b0cce17868e67a657cb20726c00bb9d90599c4f
RAGUNATH A <168917392+Blackrose143@users.noreply.github.com>
2026-09-24 14:03:22 +0530

    Time: 47 ms (59.76%), Space: 15.8 MB (66.04%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
+    bool isMatch(string s, string t) {
+        
+        int n=s.length();
+        int m=t.length();
 
-    int n,m;
-    vector<vector<int>> dp;
-    bool fun(int i,int j,string& s,string& t) {
-        if(i<0 && j<0)
-            return true;
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1));
+        dp[0][0] = true;
+        for(int i=1;i<=n;i++)
+            dp[i][0] = false;
 
-        if(j<0 && i>=0)
-            return false;
-
-        if(i<0 && j>=0) {
-            for(int k=0;k<=j;k++)
-                if(t[k]!='*')
-                    return false;
-            return true;
+        for(int i=1;i<=m;i++) {
+            bool flag = true;
+            for(int k=0;k<i;k++) {
+                if(t[k]!='*') {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][i] = flag;
         }
 
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-
-        bool ans=false;
-        if(s[i]==t[j] || t[j]=='?')
-            ans |= fun(i-1,j-1,s,t);
-        else if(t[j]=='*')
-            ans |= (fun(i-1,j,s,t) | fun(i,j-1,s,t));
-        return dp[i][j] = ans;
-    }
-
-    bool isMatch(string s, string t) {
-        n = s.size();
-        m = t.size();
-
-        dp.assign(n,vector<int>(m,-1));
-        return fun(n-1,m-1,s,t);
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                if(s[i-1]==t[j-1] || t[j-1]=='?')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(t[j-1]=='*')
+                    dp[i][j] = (dp[i-1][j] || dp[i][j-1]);
+                else
+                    dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
     }
 };
